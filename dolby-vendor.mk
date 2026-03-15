2ada55d771d27759ecb1b7ef556cc3954673ff06
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-03-15 13:09:55 +0600

    Revert "dolby: Drop 32-bit support"
    Legacy devices like redwood need 32bit. Otherwise, the effects won't load and Dolby
    will simply crash.
    
    This reverts commit 7a4cc0156353734f5e3183880505aff7e86a8979.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- dolby-vendor.mk
+++ dolby-vendor.mk
@@ -12,21 +12,21 @@ PRODUCT_COPY_FILES += \
     vendor/sony/dolby/proprietary/vendor/etc/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
 
 PRODUCT_PACKAGES += \
-    libcodec2_soft_ac4dec \
-    libcodec2_soft_ddpdec \
-    libcodec2_soft_dolby \
-    libcodec2_store_dolby \
     libdapparamstorage \
-    libdeccfg \
-    libdlbdsservice \
     libdlbpreg \
-    liboem_specific \
     libdlbvol \
     libswdap \
     libswgamedap \
     libswvqe \
-    vendor.dolby.hardware.dms@2.0-impl \
     vendor.dolby.hardware.dms@2.0 \
+    libcodec2_soft_ac4dec \
+    libcodec2_soft_ddpdec \
+    libcodec2_soft_dolby \
+    libcodec2_store_dolby \
+    libdeccfg \
+    libdlbdsservice \
+    liboem_specific \
+    vendor.dolby.hardware.dms@2.0-impl \
     vendor.dolby.hardware.dms@2.0_system \
     DolbySound \
     daxService \
